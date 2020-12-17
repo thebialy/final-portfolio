e5e589929f6ecc82d66486ea7b08ac6e0fce2104
thebialy <67696052+thebialy@users.noreply.github.com>
2020-12-17 17:19:36 -0600

    initial portfolio commit

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <footer className="contactMeContainer" id="contactMeContainer">
+        <h3>Andrew Hitchcock </h3>
+        <h3>Email: thebialy@gmail.com</h3>
+        <h3>Phone : 615-944-9872</h3>
+    </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/hero.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+const Hero = () => {
+    return (
+      <div id="hero" >
+            <img src="" class="mePicture"></img>
+        <h1 className="title">
+          <span>Hello, I'm Andrew Hitchcock</span><br />
+        </h1>
+      
+        <h2 className="whatIdo">
+      I'm a Full Stack Software Engineer<br/>Welcome to my portfolio
+        </h2>
+        <h3 className="techSkills"> Technical Skills:</h3>
+        <h4 className="Skills">
+        Javascript | Jquery | HTML5 | Node.JS | CSS3| ES6 | EJS | Mongoose| Bootstrap | Express | React | MongoDB | Heroku | GitHub | Ruby | Rails | SQL | Postgres | Postman | ​Netlify
+        </h4>
+      </div>
+    )
+  }
+
+export default Hero
