a9f76e3a12380e404b1ee283ab8481ff037dec87
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-11 16:13:45 -0600

    more cleaning

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,9 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header>
     <nav className="topNav">
-      <a className="topLinks" target= "_blank" href="https://github.com/thebialy"> Github</a>
-      <a className="topLinks" target= "_blank"href="https://www.linkedin.com/in/andrew-tyler-hitchcock/"> Linkedin</a>
-      <a className="topLinks"target= "_blank"href="https://docs.google.com/document/d/e/2PACX-1vRQX0uw6MGwNZ1GZAUOUehb0xEBMEwuEjoAAGctIh2KyhxW0AWFvJ7Bqg3dEwCdZA63mS-es-xMdKkk/pub">Resume</a>
+      <a className="topLinks" href="https://github.com/thebialy"> Github</a>
+      <a className="topLinks" href="https://www.linkedin.com/in/andrew-tyler-hitchcock/"> Linkedin</a>
+      <a className="topLinks" href="https://docs.google.com/document/d/e/2PACX-1vRQX0uw6MGwNZ1GZAUOUehb0xEBMEwuEjoAAGctIh2KyhxW0AWFvJ7Bqg3dEwCdZA63mS-es-xMdKkk/pub">Resume</a>
       <a className="topLinks"href="#contactMeContainer">Contact Me</a>
     </nav>
   </header>
