80b22977e724326f49ab338112d0d868bfab48d6
thebialy <67696052+thebialy@users.noreply.github.com>
2021-04-19 16:04:40 -0500

    formatting

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,12 @@ import React from "react"
 const Footer = () => {
     return (
         <footer className="contactMeContainer" id="contactMeContainer">
-            <h3>Andrew Hitchcock </h3>
-            <h3>Email: thebialy@gmail.com</h3>
+            <h3>
+                Andrew Hitchcock 
+            </h3>
+            <h3>
+                Email: thebialy@gmail.com
+            </h3>
     </footer>
     )
 }
