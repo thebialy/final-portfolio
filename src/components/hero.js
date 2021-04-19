429591a3dac336ad2e19712bbe0247e16d2de52b
thebialy <67696052+thebialy@users.noreply.github.com>
2021-04-19 16:04:02 -0500

    more code cleaning

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,13 +8,11 @@ const Hero = () => {
             <span>Hello, I'm Andrew Hitchcock</span>
           </h1>
           <h2 className="whatIdo">
-        Full Stack Software Engineer
+            Full Stack Software Engineer
           </h2>
-          
           <h3 className="techSkills"> Technical Skills:</h3>
           <h4 className="skills">
           Javascript | Jquery | HTML5 | Node.JS | CSS3| ES6 | EJS | Mongoose| Bootstrap | Express | React | MongoDB | Heroku | GitHub | Ruby | Rails | SQL | Postgres | Postman | ​Netlify
-          
           </h4>
       </div>
     )
